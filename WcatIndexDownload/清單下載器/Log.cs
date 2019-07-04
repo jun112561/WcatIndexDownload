19e55991e9c5f65de6e6f850d9e7a19a893d39ab
jun112561 <f20001012@Gmail.com>
2019-07-04 23:40:42 +0800

    Update to v2
    https://ci.appveyor.com/project/Perfare/assetstudio/branch/master/artifacts
    AssetStudioGUI\bin\x64\AssetStudio.x64.v0.12.65.zip
    直接引用dll，無另外修改

---
+++ WcatIndexDownload/清單下載器/Log.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace 清單下載器
+{
+    public partial class Log : Form
+    {
+        public bool isColse = true;
+
+        public Log()
+        {
+            InitializeComponent();
+        }
+
+        private void Log_Load(object sender, EventArgs e)
+        {
+            isColse = false;
+        }
+
+        private void Log_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            isColse = true;
+            Hide();
+            e.Cancel = true;
+        }
+        
+        private void Log_Activated(object sender, EventArgs e)
+        {
+            isColse = false;
+        }
+    }
+}
---
+++ WcatIndexDownload/清單下載器/Static Function.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace 清單下載器
+{
+    static class Static_Function
+    {
+        public static Bitmap ResizeImage(this Image image, int width, int height)
+        {
+            Rectangle destRect = new Rectangle(0, 0, width, height);
+            Bitmap bitmap = new Bitmap(width, height);
+            bitmap.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                using (ImageAttributes imageAttributes = new ImageAttributes())
+                {
+                    imageAttributes.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, imageAttributes);
+                }
+            }
+            return bitmap;
+        }
+
+        public static bool ComparisonFileName(this string fileName)
+        {
+            try
+            {
+                string tempFileName = "";
+                foreach (char item in fileName)
+                {
+                    if ((item >= 48 && item <= 57) || (item >= 65 && item <= 90) || item == 95 || (item >= 97 && item <= 122))
+                        tempFileName += item;
+                }
+                return fileName == tempFileName;
+            }
+            catch (Exception) { return false; }
+        }
+    }
+}
